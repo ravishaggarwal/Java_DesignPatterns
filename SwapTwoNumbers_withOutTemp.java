f9938ad76d6784239e64b7613bb5225642f2c000
Ravish Aggarwal <ravish.aggarwal@gmail.com>
2017-11-23 11:39:50 +0530

    Basis program to swap two numbers
    Basis program to swap two numbers with out using temp variable

---
+++ SwapTwoNumbers_withOutTemp.java
@@ -0,0 +1,27 @@
+package mytest.myexample;
+
+
+
+public class SwapTwoNumbers_withOutTemp {
+ 
+public static void main(String[] args) {
+ 
+int number1=20;
+int number2=30;
+ 
+System.out.println("Before Swapping");
+System.out.println("Value of number1 is :" + number1);
+System.out.println("Value of number2 is :" +number2); 
+ 
+number1=number1+number2;
+System.out.println("updated value of number1 is :" + number1);
+number2=number1-number2;
+System.out.println("updated value of number2 is :" + number2);
+number1=number1-number2;
+ 
+System.out.println("After Swapping");
+System.out.println("Value of number1 is :" + number1);
+System.out.println("Value of number2 is :" +number2);
+ 
+}
+}
